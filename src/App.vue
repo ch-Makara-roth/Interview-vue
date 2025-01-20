94145e622668b3927a7225d51bf0eafdf7c03198
Makara_Roth <chhuonmakararoth@gmail.com>
2025-01-20 09:20:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LayoutSection from './Layout/LayoutSection.vue';
+
+</script>
+
+<template>
+  <LayoutSection />
+</template>
+
+<style scoped>
+</style>
---
+++ src/Layout/LayoutSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+import  HeaderSection from '@/Layout/HeaderSection.vue'
+import FooterSection from '@/Layout/FooterSection.vue'
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <div>
+    <HeaderSection />
+    <main>
+      <RouterView />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<style>
+</style>
