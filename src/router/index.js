98f95a89de7f7a6f004f91967940a95e8a16078e
Makara_Roth <chhuonmakararoth@gmail.com>
2025-01-20 10:59:37 +0700

    add todo task

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       name: 'about',
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/task-board',
+      name: 'task-board',
+      component: () => import('../views/TaskBoardView.vue'),
+    }
   ],
 })
 
